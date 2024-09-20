6e6793e467cba27c27bd049caed0b43db7346921
woolf1717 <Barstpiczynski@gmail.com>
2024-09-20 19:21:56 +0200

    refactor: Update button.json to use new color alias for text color

--- examples/module4/lesson2/design-tokens/output.test.ts
+++ examples/module4/lesson2/design-tokens/output.test.ts
@@ -1,23 +1,18 @@
-import { describe, expect, test } from 'vitest';
 import {
   ButtonTextColor,
   ColorAliasForeground1,
   ColorGlobalGrey14,
 } from './dist/js/tokens';
+import { describe, expect, test } from 'vitest';
 
 describe('Design Tokens library output', () => {
   test('should have a color.global.grey.14 token', () => {
     expect(ColorGlobalGrey14).toBeDefined();
-    expect(ColorGlobalGrey14).toBe('#242424');
-  });
-
-  test('should have a color.alias.foreground.1 token', () => {
-    expect(ColorAliasForeground1).toBeDefined();
-    expect(ColorAliasForeground1).toBe('#242424');
+    expect(ColorGlobalGrey14).toBe('#232323');
   });
 
   test('should have a button.text.color token', () => {
     expect(ButtonTextColor).toBeDefined();
-    expect(ButtonTextColor).toBe('#242424');
+    expect(ButtonTextColor).toBe('#232323');
   });
 });
